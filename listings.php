9f606d50c7e9a26b98c44a2c6fb392c2c1ac4ceb
HelenNguyenCS <myhiep2910@yahoo.com>
2018-05-08 10:59:00 +1000

    Added new listing files which now has age, gender, and has the refactor for test

--- listings.php
+++ listings.php
@@ -4,24 +4,21 @@
 <html lang="en">
 
 <head>
-   <meta charset="utf-8" />
-   <meta name="description" content="user details" />
-   <meta name="keywords" content="exercise, athletic ability, location" />
-   <meta name="author" content="Hiep Nguyen"  />
-   <title>Listing</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Exercise app" />
+    <title>Listing</title>
+    <link rel="icon" type="image/png" href="images/favicon.svg" />
+    <link rel="stylesheet" href="styles/style.css" />
 </head>
 
 <body>
+    <?php include 'header.php'; ?>
 
-<?php include 'header.php'; ?>
-<?php 
-
-?>
 <header>
   <h1>Details About Your Exercise </h1>
 </header>
 
-       <form id="listing" method="post" action="LoadListings.php" novalidate="novalidate">
+       <form id="listing" method="post" action="load-listings.php" novalidate="novalidate">
               
             <fieldset id="details">
              <legend>Please enter your exercise details</legend>
@@ -34,6 +31,20 @@
                 <p><label for="location">Location</label>
                   <input type="text" name="Location" id="location" maxlength="20" size="20"/>
                 </p>
+
+                 <p><span id="gender"> Your gender is<br/>
+                  <label><input type="radio" name="gender" id ="male" value="male"/>Male</label>
+                    <label><input type="radio" name="gender" id ="female" value="female"/>Female</label>
+                    <label><input type="radio" name="gender"  id="other" value="other"/>Other</label>
+                  </span></p>
+
+                   <p><span id="age"> Your age is<br/>
+                  <label><input type="radio" name="age" id ="18-25" value="18-25"/>18-25</label>
+                    <label><input type="radio" name="age" id ="26-33" value="26-33"/>26-33</label>
+                    <label><input type="radio" name="age"  id="34-42" value="34-42"/>34-42</label>
+                    <label><input type="radio" name="age"  id=">42" value=">42"/> >42 </label>
+                  </span></p>
+
                <p>Blurb details about your exercise</p>
            <p><label><textarea name="blurbDetails" id="blurbDetails" rows="10" cols="80"></textarea></label></p>
                
