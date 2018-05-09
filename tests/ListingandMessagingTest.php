815867a17b7ef3748c4821e2c6d79e0aacae0431
maxcb97 <maxcb97@hotmail.com>
2018-05-09 22:12:52 +1000

    Fixed, added tests.

--- tests/ListingandMessagingTest.php
+++ tests/ListingandMessagingTest.php
@@ -20,9 +20,8 @@ class TestListingAndMessaging extends PHPUnit\Framework\TestCase
 
      public function testConnection()
     {
-        $result = connection("127.0.0.1","root","Thi2104#", "EOI");
+        $result = connection();
         $this->assertNotNull($result, "connection failed");
-     return $conn;
    }
    
    public function testInsertInputToListingTable()
@@ -49,10 +48,65 @@ class TestListingAndMessaging extends PHPUnit\Framework\TestCase
    public function testUpdateMessage() 
    {
         $conn = connection();
-        $result = getMessages($conn, '48', "accepted");
+        $result = updateMessage($conn, '48', "accepted");
         $this->assertNotNull($result, "can not update status of this message");
     
-} 
+    }
+    public function testGetMessageQuery()
+    {
+        $conn = connection();
+        $result = getMessages($conn, "test");
+        $this->assertEquals(mysqli_query($conn, "select * from messages m, exerciseDetails e where m.postnumber = e.postNumber and (m.mfrom = 'test' or m.mto = 'test')"), $result);
+    }
+
+    public function testUpdateMessageQuery()
+    {
+        $conn = connection();
+        $result = updateMessage($conn, '48', "accepted");
+        $this->assertEquals(mysqli_query($conn, "update messages set status = 'accepted' where messageId = 48"), $result);
+    }
+
+    public function testGetExerciseData()
+    {
+        $_POST["Exercise"] = "Walking";
+        $result = getExerciseData();
+        $this->assertEquals("Walking", $result);
+    }
+
+    public function testGetAthleticData()
+    {
+        $_POST["Athletic"] = "Fit";
+        $result = getAthleticData();
+        $this->assertEquals("Fit", $result);
+    }
+
+    public function testGetLocation()
+    {
+        $_POST["Location"] = "Melbourne";
+        $result = getLocation();
+        $this->assertEquals("Melbourne", $result);
+    }
+
+    public function testGetGender()
+    {
+        $_POST["gender"] = "Male";
+        $result = getGender();
+        $this->assertEquals("Male", $result);
+    }
+
+    public function testGetAge()
+    {
+        $_POST["age"] = "59";
+        $result = getAge();
+        $this->assertEquals("59", $result);
+    }
+
+    public function testGetBlurbDetails()
+    {
+        $_POST["blurbDetails"] = "Seeking person who won't collapse on five minute jog.";
+        $result = getBlurbDetails();
+        $this->assertEquals("Seeking person who won't collapse on five minute jog.", $result);
+    }
 
     
 }
