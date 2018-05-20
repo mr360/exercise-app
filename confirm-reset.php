da79043e39067e10ee1b861ecea1f7b523a89b24
MrMe360 <qd0097@gmail.com>
2018-05-20 11:58:04 +0000

    Fixed echo issue, and removed duplicate redir func

--- confirm-reset.php
+++ confirm-reset.php
@@ -5,18 +5,6 @@
 // Can be changed if we switch the DB to using some form of hashing for passwords.
 include 'create-user.php';
 
-
-function redir($message, $location)
-{
-    echo '<script language="javascript">';
-
-    echo 'alert("'.$message.'")';
-
-    echo '</script>';
-
-    echo "<script>document.location='$location'</script>";
-}
-
 function runquery($query)
 {
     require_once("settings.php");
@@ -29,9 +17,9 @@ function runquery($query)
     $result = mysqli_query($conn, $query);
 
     if (!$result) {
-        echo redir("Failed to reset password. Please try again.", "password-reset.php");
+        redir("Failed to reset password. Please try again.", "password-reset.php");
     } else {
-        echo redir("Success.", "login.php");
+        redir("Success.", "login.php");
     }
     mysqli_free_result($result);
     mysqli_close($conn);
@@ -45,15 +33,15 @@ function resetpass($username, $password, $confpassword)
         $query = "update users set password = '$hash' where username = '$username'";
         return $query;
     } else {
-        echo redir("Passwords did not match. Please try again.", "password-reset.php");
+        redir("Passwords did not match. Please try again.", "password-reset.php");
     }
 }
 
 
 
-$username = echo sanitise($input($_POST["username"]);
-$password = echo sanitise($_POST["password"]);
-$confpassword = echo sanitise($_POST["confpassword"]);
+$username = sanitise($input($_POST["username"]);
+$password = sanitise($_POST["password"]);
+$confpassword = sanitise($_POST["confpassword"]);
 
 $query = resetpass($username, $password, $confpassword);
 if ($query != null)
