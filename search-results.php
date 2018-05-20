372664479b9b8cc76735ba28b027722e661f675d
MrMe360 <qd0097@gmail.com>
2018-05-20 12:20:58 +0000

    removed duplicate viewListing func

--- search-results.php
+++ search-results.php
@@ -18,7 +18,10 @@
 
 <body>
 
-<?php include 'header.php'; ?>
+<?php 
+include 'header.php'; 
+include 'view-listings.php';
+?>
 
 
 <header>
@@ -64,99 +67,6 @@ function generateQuery()
     }
 
     return $query;
-}
-
-function viewListing()
-{
-    require("settings.php");
-    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
-    if (!$conn)
-    {
-        echo "<p>Database connection failure</p>";
-    }
-    else
-    {
-
-        $sql_table="messages";
-        $query = generateQuery();
-        $result = mysqli_query($conn, $query);
-
-        if(!$result)
-        {
-            echo "<p>Something is wrong with", $query, "</p>";
-        }
-        else
-
-        {
-
-            $resultsprinted = false;
-
-            while ($row = mysqli_fetch_assoc($result)){
-
-                $resultsprinted = true;
-
-                echo "<section class = \"sec1\">\n";
-
-                echo "<div class = \"div1\">\n";
-                echo "<p><strong>Exercise:</strong></p>",$row["Exercise"],"\n";
-
-                echo "</div>\n";
-
-                echo "<div class = \"div2\">\n";
-                echo "<p><strong>Athletic:</strong></p>",$row["Athletic"],"\n";
-
-                echo "</div>\n";
-
-                echo "<div class = \"div3\">\n";
-                echo "<p><strong>Location:</strong></p>",$row["Location"],"\n";
-
-                echo "</div>\n";
-
-                echo "<div class = \"div4\">\n";
-
-                echo "<p><strong>Gender:</strong></p>",$row["Gender"],"\n";
-
-                echo "</div>\n";
-
-                echo "<div class = \"div5\">\n";
-
-                echo "<p><strong>Age:</strong></p>",$row["Age"],"\n";
-
-                echo "</div>\n";
-                echo "<div class = \"div6\">\n";
-
-                echo "<p class='button'><a href=create-message.php?postnumber=",$row["postNumber"],">Send Message</a> </p>
-                  <p class='button'><a href=user-profile.php?postnumber=",$row["postNumber"],">See Profile</a> </p>
-                  <p> Time post: ",$row["create_time"],"</p>\n";;
-
-                echo "</div>\n";
-
-
-                echo "<div class = \"div7\">\n";
-
-                echo "<p><strong>BlurbDetails:</strong></p>",$row["BlurbDetails"],"\n";
-
-                echo "</div>\n";
-
-
-                echo "</section>\n";
-
-
-
-            }
-
-            if (!$resultsprinted)
-            {
-                echo "<p>No results found. Please try expanding your criteria, and try again.</p>";
-            }
-
-            mysqli_free_result($result);
-
-
-        }
-
-        mysqli_close($conn);
-    }
 }
 
 viewListing();
